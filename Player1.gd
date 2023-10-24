693531e1518eac1714a5c9a3445c5a7876391072
Anders Holthe Stanger <Andersstanger@gmail.com>
2023-10-24 10:08:17 +0200

    Wall-jump på høyre vegg
    Lagt til timer som stopper deg ifra å spamme for å hoppe oppover samme vegg. Har også lagt til en boolean som sier om du er i et hopp eller ikke.

--- Player1.gd
+++ Player1.gd
@@ -1,28 +1,41 @@
 extends CharacterBody2D
 
+ 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -450.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+const JUMP_VELOCITY = -500.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var Direction = "Right"
+var jumpDelay = false
 
 
 func _physics_process(delta):
-	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+ 
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("p1jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	if Input.is_action_just_pressed("p1jump"):
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		elif is_on_wall():
+			velocity.y = JUMP_VELOCITY - 100
+			velocity.x = JUMP_VELOCITY - 250
+			$Timer.start()
+			jumpDelay = true
+			await $Timer.timeout
+			print("es")
+			jumpDelay = false
+	
+	
 	var direction = Input.get_axis("p1left", "p1right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if	jumpDelay == false:
+		if direction:
+			velocity.x = direction * SPEED
+		elif is_on_floor():
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+		else:
+			velocity.x = move_toward(velocity.x, 0, 50)
+
+ 
 
 	move_and_slide()
