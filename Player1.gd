105f48aa7c725a39afab922bfba2a33de574e3f5
Anders Holthe Stanger <Andersstanger@gmail.com>
2023-10-24 11:24:36 +0200

    lagt til venstre wall-jump
    du kan nå wall-jumpe på alle vegger

--- Player1.gd
+++ Player1.gd
@@ -3,35 +3,48 @@ extends CharacterBody2D
  
 
 const SPEED = 300.0
+const AIR_SPEED = 50
 const JUMP_VELOCITY = -500.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var Direction = "Right"
 var jumpDelay = false
+var whichWall = ""
 
 
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
+	# Collide with right wall
 	if $RayCastRight.is_colliding():		
-		print($RayCastRight.get_collider().name)	
-		if $RayCastRight.get_collider().name == "Wall":
-			print("collided with wall")
+#		print($RayCastRight.get_collider().name)	
+		if $RayCastRight.get_collider().name.find("wall"):
+			whichWall = "right"
+			
+	# Collide with left wall
 	elif $RayCastLeft.is_colliding():		
-		print($RayCastLeft.get_collider().name)	
-		if $RayCastLeft.get_collider().name == "Wall2":
-			print("collided with wall2")
+#		print($RayCastLeft.get_collider().name)	
+		if $RayCastLeft.get_collider().name.find("wall"):
+			whichWall = "left"
 
 	if Input.is_action_just_pressed("p1jump"):
 		if is_on_floor():
 			velocity.y = JUMP_VELOCITY
-		elif is_on_wall():
+		elif is_on_wall() and whichWall == "right":
 			velocity.y = JUMP_VELOCITY - 200
-			velocity.x = JUMP_VELOCITY - 50
+			velocity.x = JUMP_VELOCITY - AIR_SPEED
 			$Timer.start()
 			jumpDelay = true
 			await $Timer.timeout
-			print("es")
+			print("RIGHT")
+			jumpDelay = false
+		elif is_on_wall() and whichWall == "left":
+			velocity.y = JUMP_VELOCITY - 200
+			velocity.x = JUMP_VELOCITY *-1 + AIR_SPEED
+			$Timer.start()
+			jumpDelay = true
+			await $Timer.timeout
+			print("LEFT")
 			jumpDelay = false
 
 	
@@ -42,7 +55,7 @@ func _physics_process(delta):
 		elif is_on_floor():
 			velocity.x = move_toward(velocity.x, 0, SPEED)
 		else:
-			velocity.x = move_toward(velocity.x, 0, 50)
+			velocity.x = move_toward(velocity.x, 0, AIR_SPEED)
 
 
 	move_and_slide()
