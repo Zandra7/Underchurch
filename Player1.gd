4dfaeeeffe93d3286be49a459f06a41bf632a9ab
Anders Holthe Stanger <Andersstanger@gmail.com>
2023-10-24 10:49:57 +0200

    La til raycast2D
    Spiller vet nå om man treffer vegg på høyre eller venstre side.

--- Player1.gd
+++ Player1.gd
@@ -12,20 +12,28 @@ var jumpDelay = false
 func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
- 
+
+	if $RayCastRight.is_colliding():		
+		print($RayCastRight.get_collider().name)	
+		if $RayCastRight.get_collider().name == "Wall":
+			print("collided with wall")
+	elif $RayCastLeft.is_colliding():		
+		print($RayCastLeft.get_collider().name)	
+		if $RayCastLeft.get_collider().name == "Wall2":
+			print("collided with wall2")
 
 	if Input.is_action_just_pressed("p1jump"):
 		if is_on_floor():
 			velocity.y = JUMP_VELOCITY
 		elif is_on_wall():
-			velocity.y = JUMP_VELOCITY - 100
-			velocity.x = JUMP_VELOCITY - 250
+			velocity.y = JUMP_VELOCITY - 200
+			velocity.x = JUMP_VELOCITY - 50
 			$Timer.start()
 			jumpDelay = true
 			await $Timer.timeout
 			print("es")
 			jumpDelay = false
-	
+
 	
 	var direction = Input.get_axis("p1left", "p1right")
 	if	jumpDelay == false:
@@ -36,6 +44,7 @@ func _physics_process(delta):
 		else:
 			velocity.x = move_toward(velocity.x, 0, 50)
 
- 
 
 	move_and_slide()
+
+
