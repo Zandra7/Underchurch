e036c89c3bda7a097df258566ce6c177a3b095d8
Anders Holthe Stanger <Andersstanger@gmail.com>
2023-11-03 11:28:53 +0100

    la til kollisjon på ildkule
    hvis ildkulen treffer veggen eller fiende blir den borte

--- Ildkule.gd
+++ Ildkule.gd
@@ -16,6 +16,6 @@ func _process(delta):
 
 
 func _on_area_2d_body_entered(body):
-	print(body.name)
-	if body.name != "TopDownGuy":
+	if body.name != "Player1" && body.name != "Player2":
 		queue_free()
+		
